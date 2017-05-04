5a809c22ea2ec10ef823bf6c430535094faa2b6c
Fernando Cezar <netcriptus@gmail.com>
2017-05-04 11:14:02 +0200

    only install go1.6 if we have go1.4 installed

--- basic_tools.sh
+++ basic_tools.sh
@@ -8,8 +8,13 @@ echo
 echo "#### Installing basic tools from homebrew ####"
 echo
 brew tap caskroom/cask
-brew install ack git bash-completion coreutils keybase wget python3
+brew install ack git bash-completion coreutils keybase wget pyenv
 brew cask install docker google-chrome iterm2
+
+echo "#### Installing python 3 ####"
+echo
+pyenv install 3.6.1
+pyenv global 3.6.1
 
 #Install RVM
 echo "#### Installing RVM ####"
@@ -17,11 +22,11 @@ curl -sSL https://get.rvm.io | bash -s stable
 
 # install virtualenv and virtualenvwrapper
 echo "#### Installing virtualenvwrapper ####"
-sudo pip install virtualenv virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper isort
 
 # Install Go 1.6
 echo "#### Installing GVM and golang ####"
 echo
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) &&\
 gvm install go1.4 && gvm use go1.4 &&\
 gvm install go1.6 && gvm use go1.6
