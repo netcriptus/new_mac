8c782b5ea35b96667a74fb2b4304112ebc012bba
Fernando Cezar <netcriptus@gmail.com>
2017-05-04 11:11:13 +0200

    add more extensions

--- atom.sh
+++ atom.sh
@@ -5,8 +5,8 @@ echo
 brew cask install atom
 
 GO_PKGS='gofmt gometalinter-linter navigator-godef'
-PYTHON_PKGS='autocomplete-python linter linter-pep8 python-indent'
+PYTHON_PKGS='autocomplete-python linter linter-pep8 python-indent python-isort python-tools'
 RUBY_PKGS='cucumber'
-GIT_PKGS='git-blame'
+MISC_PKGS='atom-spotify2 language-docker git-blame sort-lines busy-signal intentions'
 
-which apm >&- && apm install ${GO_PKGS} ${PYTHON_PKGS}
+which apm >&- && apm install ${GO_PKGS} ${PYTHON_PKGS} ${RUBY_PKGS} ${MISC_PKGS}
